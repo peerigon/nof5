5a717061743c9267a56d057b9a25c56bd7850beb
Paul Torka <paul.torka@yahoo.de>
2012-06-12 18:57:50 +0200

    renamed config.json to default.config, moved app.js to lib and renamed it to server as this name better fits. renamed expect to expect.js. and some other refactoring. current state is not working and in progress.

--- routes/mochaTestRunner.js
+++ routes/mochaTestRunner.js
@@ -1,35 +1,11 @@
-"use_strict";
+"use strict";
 
-var Dir = require("../lib/Directory.js"),
-    dir,
-    isModuleRootDir = false,
-    files,
-    nconf = require("nconf");
-
-
-function findPackageJSON(file, index) {
-    if (file === "package.json") {
-        isModuleRootDir = true;
-    }
-}
-
-//Initialize given directory
-dir = new Dir(process.cwd());
-
-//Read files from given directory
-files = dir.readDir();
-files.forEach(findPackageJSON);
-
-//Check if directory is a module's root directory
-if (!isModuleRootDir) {
-    throw new Error("package.json not found. You must execute nof5 within a module's root directory.");
-}
 
 //Render Mocha Test-Runner
 module.exports = function mochaTestRunner(req, res) {
     res.render("mochaTestRunner", {
         title: "Mocha Test-Runner",
-        tests: {},
+        tests: [],
         assertionSuite: "expect"
     });
 };
