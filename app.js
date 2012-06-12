babfa36d1305ced0f3facb5be3555b46b1f01004
Paul Torka <paul.torka@yahoo.de>
2012-06-12 16:51:04 +0200

    current not working state

---
+++ app.js
@@ -0,0 +1,38 @@
+"use_strict";
+
+var express = require("express"),
+    routes = require("./routes"),
+    nof5 = module.exports = express.createServer(),
+    socketIO = require("socket.io").listen(nof5);
+
+nof5.configure(function(){
+    //Setup view rendering
+    nof5.set("views", __dirname + "/views");
+    nof5.set("view engine", "jade");
+
+    //Setup middleware
+    nof5.use(express.bodyParser()); //@TODO Does nof5 need bodyParser?
+    nof5.use(express.methodOverride()); //@TODO Does nof5 need methodOverride?
+    nof5.use(nof5.router); //@TODO Does nof5 need router?
+
+        //Setup static files
+    nof5.use(express.static(__dirname + "/public"));
+});
+
+//Define route-handler
+nof5.get("/", routes.mochaTestRunner);
+nof5.get("/mocha", routes.mochaTestRunner);
+
+//Start express-server
+nof5.listen(11234, function(){
+  console.log("nof5 listening on port %d", nof5.address().port);
+});
+
+//Start socket.io-sever
+socketIO("connection", function (socket) {
+
+    socket.emit("f5");
+
+});
+
+
---
+++ public/javascripts/test_runner/mocha/mocha.setup.js
@@ -0,0 +1,5 @@
+mocha.setup({
+    ui: 'bdd'//,
+    //globals: ["global1", "global2"],
+    //timeout
+});
