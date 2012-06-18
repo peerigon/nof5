d0aa6c7354dc266c8861e19ce7674726abb4218a
Paul Torka <paul.torka@yahoo.de>
2012-06-18 11:34:39 +0200

    Added some tests.

--- test/DirectoryMasterWatcher.test.js
+++ test/DirectoryMasterWatcher.test.js
@@ -1,5 +1,4 @@
 var expect = require("expect.js"),
-    fs = require("fs"),
     _ = require("underscore"),
     DirectoryMasterWatcher = require("../lib/DirectoryMasterWatcher.js");
 
@@ -27,54 +26,41 @@ describe("#DirectoryMasterWatcher", function () {
         });
     });
 
-    describe("#api", function () {
+    describe("#methods", function () {
 
-        var nof5RootDir = __dirname + "/../",
+        var nof5RootDir = __dirname.replace("/test", ""),
             dMW = new DirectoryMasterWatcher(nof5RootDir),
-            allRootDirFiles,
-            rootDirDirectories = [],
-            rootDirFiles = [];
+            watchedDirectories = dMW.getWatchedDirectories(),
+            directoryFilter = /[\/\\]\..*$/g;
 
-        before(function (done) {
-            fs.readdir(nof5RootDir, function (err, files) {
-                allRootDirFiles = files;
+        function isValidDirectory(dirPath) {
+            return dirPath.search(directoryFilter) === -1;
+        }
 
-                function filterFiles(filePath, index) {
-                    if (fs.statSync(filePath).isFile()) {
-                        rootDirFiles.push(filePath);
-                    } else {
-                        rootDirDirectories.push(filePath);
-                    }
-                }
+        function isNodeModulesDirectory(dirPath) {
+            return dirPath === "node_modules";
+        }
 
-                _.each(allRootDirFiles, filterFiles);
-
-                done();
-            });
-        });
-
-        describe("#addOnDirChange", function () {
-
-            var getWatchedDirectoriesReturn = dMW.getWatchedDirectories();
-
-            console.log(getWatchedDirectoriesReturn);
+        describe("#getWatchedDirectories()", function () {
 
             it("should return an Array", function () {
-                expect(getWatchedDirectoriesReturn).to.be.an(Array);
+                expect(watchedDirectories).to.be.an(Array);
             });
 
-            it("should contain no path to a file", function () {
-                expect(_.difference(allRootDirFiles, rootDirFiles)).to.be.equal(getWatchedDirectoriesReturn);
+            it("should return an Array with at least one item/path to a dir", function () {
+                expect(watchedDirectories.length > 0).to.be.ok();
             });
 
-            it("should contain no hidden files or rather no files beginning with a dot(e.g. .git)", function () {
-                _.each(getWatchedDirectoriesReturn, function (dirPath, index) {
-                    expect(dirPath.search(/[\/\\]\..*$/g)).to.be.equal(-1);
+            it("should contain only paths to (visible/no dot as prefix) directories", function () {
+                _.each(watchedDirectories, function (dirPath) {
+                    expect(isValidDirectory(dirPath)).to.be.ok();
                 });
             });
 
-            it("should contain only paths to directories", function () {
-
+            it("should not contain any node_modules directory", function () {
+                _.each(watchedDirectories, function (dirPath) {
+                    expect(isNodeModulesDirectory(dirPath)).not.to.be.ok();
+                });
             });
 
         });
