58eadfa3f93fca480404bfed0407942f9a2d5755
Paul Torka <paul.torka@yahoo.de>
2012-06-19 14:41:53 +0200

    Current probably working state of refactoring.

--- test/DirectoryMasterWatcher.test.js
+++ test/DirectoryMasterWatcher.test.js
@@ -7,46 +7,94 @@ var expect = require("expect.js"),
 
 describe("#DirectoryMasterWatcher", function () {
 
+    var dummyDirectory = __dirname + "/dummyDirectory/";
+
+
+
     describe("#constructor", function () {
         it("should be a Function", function () {
             expect(DirectoryMasterWatcher).to.be.a(Function);
         });
 
-        it("should throw an Error if no directory path given", function () {
-            expect(function () {
-                new DirectoryMasterWatcher();
-            }).to.throwError();
-        });
-
-        it("should throw an Error if path to a file is given", function () {
-            expect(function () {
-                new DirectoryMasterWatcher(__filename);
-            }).to.throwError();
-        });
-
         it("should be a DirectoryMasterWatcher", function () {
-            expect(new DirectoryMasterWatcher(__dirname)).to.be.a(DirectoryMasterWatcher);
+            expect(new DirectoryMasterWatcher()).to.be.a(DirectoryMasterWatcher);
         });
 
         it("should inherit from EventEmitter", function () {
-            expect(new DirectoryMasterWatcher(__dirname)).to.be.an(EventEmitter);
+            expect(new DirectoryMasterWatcher()).to.be.an(EventEmitter);
         });
+    });
+
+    describe("#watch", function () {
+
+        var directoryMasterWatcher;
+
+        beforeEach(function () {
+            directoryMasterWatcher = new DirectoryMasterWatcher();
+        });
+
+        describe("#Error-Handling", function () {
+
+            it("should throw an Error if no directory path given", function () {
+                expect(function () {
+                    directoryMasterWatcher.watch();
+                }).to.throwError();
+            });
+
+            it("should throw an Error if path to a file is given", function () {
+                expect(function () {
+                    directoryMasterWatcher.watch(__filename);
+                }).to.throwError();
+            });
+
+        });
+
+        describe("#second call", function () {
+            it("should close all existing watchers on a second valid call to watch() and create new ones", function (done) {
+                fs.mkdir(dummyDirectory, function (error) {
+                    if (error) {
+                        throw error;
+                    }
+
+                    directoryMasterWatcher.watch(__dirname);
+                    directoryMasterWatcher.on("change", function () {
+                        throw new Error("Test failed. Close was called an this listener should be dead.");
+                    });
+
+                    directoryMasterWatcher.close();
+
+                    directoryMasterWatcher.watch(__dirname);
+                    directoryMasterWatcher.once("change", function () {
+                        done();
+                    });
+
+                    fs.rmdir(dummyDirectory, function (error) {
+                        if (error) {
+                            throw error;
+                        }
+                    });
+
+                });
+            });
+        });
+
     });
 
     describe("#Events", function () {
 
-        var directoryMasterWatcher,
-            nof5RootDir = path.resolve(__dirname + "/../"),
-            dummyDirectory = __dirname + "/dummyDirectory/";
+        var directoryMasterWatcher;
 
         beforeEach(function (done) {
-            directoryMasterWatcher = new DirectoryMasterWatcher(nof5RootDir);
+            directoryMasterWatcher = new DirectoryMasterWatcher();
+
 
             fs.mkdir(dummyDirectory, function (error) {
 
                 if (error && error.code !== "EEXIST") {
                     throw error;
                 }
+
+                directoryMasterWatcher.watch(__dirname);
 
                 done();
             });
@@ -56,9 +104,9 @@ describe("#DirectoryMasterWatcher", function () {
             directoryMasterWatcher.close();
         });
 
-        describe("should not throw an 'dirChange'-Event after a call of close()", function () {
+        describe("#change-Event", function () {
 
-            it("should not throw an 'dirChange'-Event after a call of close()", function (done) {
+            it("should NOT throw an 'change'-Event after a call of close()", function (done) {
                 directoryMasterWatcher.on("change", function () {
                     throw new Error("Execution of 'change'-Event-Listener after call to close: Test Failed.");
                 });
