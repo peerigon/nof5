30fead47449a902719f1537a11d9de3710e3d45c
topa <paul.torka@yahoo.de>
2012-07-06 14:13:57 +0200

    Added a browserifyMiddleware hook.

--- test/test_scenario/nof5.hooks.js
+++ test/test_scenario/nof5.hooks.js
@@ -1,7 +1,17 @@
 "use strict";
 
-exports.before = function (callback) {
+exports.browserifyMiddleware = function () {
+    return [function () {
+        //console.log("browsierfyMiddleware 1");
+    }, function () {
+        //console.log("browsierfyMiddleware 2");
+    }];
+};
+
+exports.beforeEach = function (callback) {
     if (typeof callback === "function") {
         callback();
     }
+
+    //console.log("hook: beforeEach");
 };
