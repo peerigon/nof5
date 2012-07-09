c24e5f848e0543a3b291590cf55e6a8dcbed6691
topa <paul.torka@yahoo.de>
2012-07-09 11:25:31 +0200

    Added a simple Bundler called NOF5.

---
+++ test/bundler/NOF5.test.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var _ = require("underscore"),
+    path = require("path"),
+    expect = require("expect.js"),
+    getTestScripts = require("../mocks/helpers/getTestScripts.js"),
+    testScripts = getTestScripts(path.resolve(__dirname + "/../test_scenario/"));
+
+var NOF5Bundler = require("../../lib/bundler/NOF5.js");
+
+describe("NOF5", function () {
+
+    var bundler;
+
+    beforeEach(function () {
+        bundler = new NOF5Bundler();
+    });
+
+    describe("# get()", function () {
+
+        it("should return a string", function () {
+            expect(typeof bundler.get(testScripts)).to.be.equal("string");
+        });
+
+        it("should contain each given test-script", function () {
+
+            var bundle = bundler.get(testScripts);
+
+            _(testScripts).each(function testScriptsIterator (testScript) {
+                expect(bundle.search("//@ sourceURL=" + testScript) > -1).to.be(true);
+            });
+        });
+
+    });
+
+});
