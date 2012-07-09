9fad6c0e92fa2bb53c314cf286f212c3a6c8c244
topa <paul.torka@yahoo.de>
2012-07-09 10:56:35 +0200

    Added the browserify Bundler

---
+++ test/bundler/Browserify.test.js
@@ -0,0 +1,94 @@
+"use strict";
+
+var expect = require("expect.js"),
+    path = require("path"),
+    getTestScripts = require("../mocks/helpers/getTestScripts.js"),
+    testScripts = getTestScripts(path.resolve(__dirname + "/../test_scenario"));
+
+
+var BrowserifyBundler = require("../../lib/bundler/Browserify.js");
+
+describe("BrowserifyBundler", function () {
+
+    var bundler;
+
+    beforeEach(function () {
+        bundler = new BrowserifyBundler();
+    });
+
+    describe("# get()", function () {
+
+        it("should return a string", function () {
+           expect(typeof bundler.get(testScripts)).to.be.equal("string");
+        });
+
+    });
+
+    describe("# use", function () {
+
+        it("should use given middleware for bundling", function (done) {
+            bundler.use(function () {
+                done();
+            });
+            bundler.get();
+        });
+
+        it("should also except a collection of middleware as an array", function (done) {
+            bundler.use([
+                function () {
+                    //Do nothing
+                },
+                function () {
+                    done();
+                }
+            ]);
+            bundler.get();
+        });
+
+        it("should use middleware in the given order", function () {
+            var predictedOrder = [1, 2, 3],
+                executionOrder = [];
+            bundler.use(function () {
+                executionOrder.push(1);
+            });
+            bundler.use([
+                function () {
+                    executionOrder.push(2);
+                },
+                function () {
+                    executionOrder.push(3);
+                }
+            ]);
+            bundler.get();
+
+            expect(executionOrder[0]).to.be.equal(predictedOrder[0]);
+            expect(executionOrder[1]).to.be.equal(predictedOrder[1]);
+            expect(executionOrder[2]).to.be.equal(predictedOrder[2]);
+        });
+
+        it("should return a reference to itself", function () {
+            expect(bundler.use()).to.be.equal(bundler);
+        });
+
+    });
+
+    describe("# ignore()", function () {
+
+        /*
+        it("should ignore given files", function () {
+            var bundle;
+
+            bundler.ignore();
+            bundle = bundler.get(testScripts);
+            expect(bundle.search(/ /g)).to.be.equal(-1);
+
+        });
+        */
+
+        it("should return a reference to itself", function () {
+            expect(bundler.ignore()).to.be.equal(bundler);
+        });
+
+    });
+
+});
