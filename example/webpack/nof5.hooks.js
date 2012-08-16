0c2e6a716150a25cbd063fc53a3060e860fcbe2b
topa <paul.torka@yahoo.de>
2012-08-16 18:18:18 +0200

    - added nof5.hooks - added webpack-hooks

---
+++ example/webpack/nof5.hooks.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: topa
+ * Date: 8/16/12
+ * Time: 6:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ example/webpack/nof5.webpack.js
@@ -0,0 +1,6 @@
+"use strict";
+
+exports.use = function () {
+    console.log("using webpack hooks");
+    return {};
+};
