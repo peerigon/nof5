9f5f38e3ef0010afffbb545469dbbf1619c9340a
Matthias Jahn <matthias.jahn@peerigon.com>
2012-10-02 16:23:33 +0200

    Fixed error in Config.js Changed clients.hooks

--- example/browserify/nof5.clients.hooks.js
+++ example/browserify/nof5.clients.hooks.js
@@ -6,9 +6,9 @@ exports.use = function useClientHook() {
 
     useClientHookExecuted = true;
 
-    var clients = ["Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.19" +
-            " (KHTML, like Gecko) Ubuntu/12.04 Chromium/18.0.1025.168 Chrome/18.0.1025.168 Safari/535.19"];
+    var clients = ["Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.11 (KHTML, like Gecko) " +
+            "Ubuntu/12.04 Chromium/20.0.1132.47 Chrome/20.0.1132.47 Safari/536.11"];
 
     return clients;
 };
