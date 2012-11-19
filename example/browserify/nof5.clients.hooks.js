510c5c34079bb597d5c9c8a3e4fcb93c53c8cc8b
topa <paul.torka@yahoo.de>
2012-11-19 21:12:37 +0100

    - updated userAgents to Chrome 23 and Firefox 16

--- example/browserify/nof5.clients.hooks.js
+++ example/browserify/nof5.clients.hooks.js
@@ -6,9 +6,10 @@ exports.use = function useClientHook() {
 
     useClientHookExecuted = true;
 
-    var clients = ["Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.11 (KHTML, like Gecko) " +
-            "Ubuntu/12.04 Chromium/20.0.1132.47 Chrome/20.0.1132.47 Safari/536.11"];
+    var clients = [
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0"
+    ];
 
     return clients;
 };
