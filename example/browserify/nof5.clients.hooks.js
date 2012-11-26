b354d255aca567e44bd8904798782f193f28aa57
topa <paul.torka@peerigon.com>
2012-11-26 16:26:40 +0100

    - Definition of expected clients works now much more comfortable through an object containing keys: family like Chrome, Major like 23, os like Linux

--- example/browserify/nof5.clients.hooks.js
+++ example/browserify/nof5.clients.hooks.js
@@ -6,10 +6,15 @@ exports.use = function useClientHook() {
 
     useClientHookExecuted = true;
 
-    var clients = [
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
-        "Mozilla/5.0 (X11; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0"
-    ];
+    var clients = [{
+            family: "Chrome",
+            major: 23,
+            os: "Linux"
+        }, {
+            family: "Firefox",
+            major: 17,
+            os: "Ubuntu"
+        }];
 
     return clients;
 };
